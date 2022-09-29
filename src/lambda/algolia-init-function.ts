249af96f3be764a35078c69e58711b571eed26a2
Jiri Lojda <jiril@kontent.ai>
2022-09-29 12:55:00 +0200

    Refactor netlify functions

--- src/lambda/algolia-init-function.ts
+++ src/lambda/algolia-init-function.ts
@@ -1,73 +1,73 @@
 /* The principle of modeling modular content for search lies in the SLUG property 
  * The assumption being that every item that has a SLUG property
  * is being rendered on the website as a separate page, thus pages with slug are marked
- * as the ones that are being serached for + linked items with SLUG are being ignored 
+ * as the ones that are being searched for + linked items with SLUG are being ignored
  * while the content of a page is being modeled */
 
-/* If your model does not support this assumption, you have to remodel the processing 
+/* If your model does not support this assumption, you have to remodel the processing
  * part of the function based on your model (i.e. use taxonomies instead of the slug property)
- * in order to model your data correcly inside of the search engine */
-import { APIGatewayEvent, Context } from 'aws-lambda'
+ * in order to model your data correctly inside of the search engine */
+import { Handler } from '@netlify/functions';
+import { DeliveryClient, IContentItem } from '@kontent-ai/delivery-sdk';
+import { canConvertToAlgoliaItem, convertToAlgoliaItem } from './utils/algoliaItem';
+import createAlgoliaClient from 'algoliasearch';
+import { hasStringProperty, nameOf } from './utils/typeguards';
 
-import { SearchProjectConfiguration } from "./utils/search-model"
+const { ALGOLIA_API_KEY } = process.env;
 
-import AlgoliaClient from "./utils/algolia-client";
-import KontentClient from './utils/kontent-client';
-
-// @ts-ignore - netlify env. variable
-const { ALGOLIA_API_KEY } = process.env
-
-function getConfiguration(body: string): SearchProjectConfiguration {
-  const jsonBody = JSON.parse(body);
-  const config: SearchProjectConfiguration = {
-    kontent: {
-      projectId: jsonBody.projectId,
-      language: jsonBody.language,
-      slugCodename: jsonBody.slug
-    },
-    algolia: {
-      appId: jsonBody.appId,
-      apiKey: ALGOLIA_API_KEY,
-      index: jsonBody.index
-    }
-  };
-
-  return config;
-}
-
-/* FUNCTION HANDLER */
-export async function handler(event: APIGatewayEvent, context: Context) {
-
-  // Only receiving POST requests
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+export const handler: Handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  if (!event.body)
-    return { statusCode: 200, body: [] };
+  const body = JSON.parse(event.body || 'null');
+  if (!isValidBody(body)) {
+    return { statusCode: 400, body: 'Missing or invalid body, please check the documentation' };
+  }
+  if (!ALGOLIA_API_KEY) {
+    return { statusCode: 500, body: 'Some environment variables are missing, please check the documentation' };
+  }
 
-  // parse config from the body & env. variables
-  const config = getConfiguration(event.body);
-  const kontentClient = new KontentClient(config.kontent);
+  const deliverClient = new DeliveryClient({ projectId: body.projectId });
+  const allItems = await getAllContentFromProject(deliverClient, body.language);
+  const allItemsMap = new Map(allItems.map(i => [i.system.codename, i]));
+  const recordItems = allItems
+    .filter(canConvertToAlgoliaItem(body.slug))
+    .map(convertToAlgoliaItem(allItemsMap, body.slug));
 
-  // get all content from Kontent
-  const content = await kontentClient.getAllContentFromProject();
-
-  // all items with a predefined slug property -> SEARCHABLE PAGES (indexed objects)
-  const contentWithSlug = content.filter(item => item[config.kontent.slugCodename]);
-
-  // creates a searchable structure based on the content's structure
-  const searchableStructure = kontentClient.createSearchableStructure(contentWithSlug, content);
-
-  // index the created content structure into algolia
-  const algoliaClient = new AlgoliaClient(config.algolia);
-  // setup default settings for a new index (faceting, searching, etc.)
-  await algoliaClient.setupIndex();
-
-  const indexedItems = await algoliaClient.indexSearchableStructure(searchableStructure);
+  const algoliaClient = createAlgoliaClient(body.appId, ALGOLIA_API_KEY);
+  const index = algoliaClient.initIndex(body.index);
+  await index.setSettings({
+    searchableAttributes: ['content.contents', 'content.name', 'name'],
+    attributesForFaceting: ['content.codename', 'language'],
+    attributesToSnippet: ['content.contents:80'],
+  }).wait();
+  const result = await index.saveObjects(recordItems).wait();
 
   return {
     statusCode: 200,
-    body: `${JSON.stringify(indexedItems)}`,
+    body: JSON.stringify(result.objectIDs),
   };
 };
+
+const getAllContentFromProject = async (deliverClient: DeliveryClient, languageCodename: string): Promise<IContentItem[]> => {
+  const feed = await deliverClient.items().queryConfig({ waitForLoadingNewContent: true })
+    .languageParameter(languageCodename).equalsFilter('system.language', languageCodename).toPromise();
+
+  return [...feed.data.items, ...Object.values(feed.data.linkedItems)];
+};
+
+type BodyConfig = Readonly<{
+  projectId: string;
+  language: string;
+  slug: string;
+  appId: string;
+  index: string;
+}>;
+
+const isValidBody = (body: Record<string, unknown>): body is BodyConfig =>
+  hasStringProperty(nameOf<BodyConfig>('projectId'), body) &&
+  hasStringProperty(nameOf<BodyConfig>('language'), body) &&
+  hasStringProperty(nameOf<BodyConfig>('slug'), body) &&
+  hasStringProperty(nameOf<BodyConfig>('appId'), body) &&
+  hasStringProperty(nameOf<BodyConfig>('index'), body);
