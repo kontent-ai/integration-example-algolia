07d505b4d287a0ec787e5f2fae47c5b1fda2b2a0
Jiri Lojda <jiril@MF-12-NTB.kontent.ai>
2023-02-03 12:39:39 +0100

    Improve error message when there are some missing environment variables in functions (print missing env variables into the message)

---
+++ src/functions/utils/createEnvVars.ts
@@ -0,0 +1,8 @@
+export const createEnvVars = <VarNames extends readonly string[]>(varNames: VarNames) => {
+  const envVars = Object.fromEntries(varNames.map(n => [n, process.env[n]]));
+  
+  return {
+    envVars,
+    missingEnvVars: Object.entries(envVars).filter(([, v]) => v === undefined).map(([n]) => n),
+  } as const;
+};
