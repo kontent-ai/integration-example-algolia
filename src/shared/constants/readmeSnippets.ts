074f1c02c9d13a3e290a9f75aa3d90a697a3ce0c
Jiri Lojda <jiril@MF-12-NTB.kontent.ai>
2023-03-09 15:00:36 +0100

    Create readmeSnippets.ts for snippets in readme
    - It has the advantage of ts support => enforcing all and valid config keys are present

---
+++ src/shared/constants/readmeSnippets.ts
@@ -0,0 +1,21 @@
+import { Config } from "../../ConfigContext";
+import { InitRequestBody } from "../types/initRequestBody";
+
+// Remember to update readme when you change this file.
+
+export const exampleCustomElementConfig: Config = {
+  language: "{Kontent.ai language codename}",
+  projectId: "{Kontent.ai project ID}",
+  algoliaAppId: "{Algolia app id}",
+  algoliaSearchKey: "{Algolia search-only api key}",
+  algoliaIndexName: "{Algolia index name}",
+  slugCodename: "{Kontent.ai slug codename}",
+};
+
+export const exampleInitRequestBody: InitRequestBody = {
+  language: "{Kontent.ai language codename}",
+  projectId: "{Kontent.ai project ID}",
+  algoliaAppId: "{Algolia app id}",
+  algoliaIndexName: "{Algolia index name}",
+  slugCodename: "{Kontent.ai slug codename}",
+};
